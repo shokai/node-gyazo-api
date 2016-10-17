1a1c61570afb3291cfc29129aa531a4935212ecf
Sho Hashimoto <hashimoto@shokai.org>
2016-10-18 02:15:14 +0900

    rewrite test with async-await

--- tests/test_gyazo.js
+++ tests/test_gyazo.js
@@ -16,68 +16,49 @@ describe('GYAZO_TOKEN', function () {
 describe('"upload" method', function () {
   const gyazo = new Gyazo(process.env.GYAZO_TOKEN)
 
-  it('should upload image', function () {
+  it('should upload image', async function () {
     this.timeout(10000)
-    return gyazo
-      .upload(imgPath)
-      .then((res) => {
-        assert.isObject(res.response)
-        assert.isString(res.data.image_id)
-        assert.isUrl(res.data.permalink_url)
-        assert.isUrl(res.data.url)
-      })
+    const res = await gyazo.upload(imgPath)
+    assert.isObject(res.response)
+    assert.isString(res.data.image_id)
+    assert.isUrl(res.data.permalink_url)
+    assert.isUrl(res.data.url)
   })
 
-  it('should upload image from stream', function () {
+  it('should upload image from stream', async function () {
     this.timeout(10000)
-    return gyazo
-      .upload(fs.createReadStream(imgPath))
-      .then((res) => {
-        assert.isObject(res.response)
-        assert.isString(res.data.image_id)
-        assert.isUrl(res.data.permalink_url)
-        assert.isUrl(res.data.url)
-      })
+    const res = await gyazo.upload(fs.createReadStream(imgPath))
+    assert.isObject(res.response)
+    assert.isString(res.data.image_id)
+    assert.isUrl(res.data.permalink_url)
+    assert.isUrl(res.data.url)
   })
 })
 
 describe('"list" method', function () {
   const gyazo = new Gyazo(process.env.GYAZO_TOKEN)
 
-  it('should return list of images', function () {
+  it('should return list of images', async function () {
     this.timeout(3000)
-    return gyazo
-      .list({ page: 1, per_page: 5 })
-      .then((res) => {
-        assert.isObject(res.response)
-        assert.isArray(res.data)
-        assert.lengthOf(res.data, 5)
-        assert.isString(res.data[0].image_id)
-        assert.isUrl(res.data[0].permalink_url)
-        assert.isUrl(res.data[0].url)
-        assert.equal(res.response.headers['x-current-page'], 1)
-        assert.equal(res.response.headers['x-per-page'], 5)
-      })
+    const res = await gyazo.list({ page: 1, per_page: 5 })
+    assert.isObject(res.response)
+    assert.isArray(res.data)
+    assert.lengthOf(res.data, 5)
+    assert.isString(res.data[0].image_id)
+    assert.isUrl(res.data[0].permalink_url)
+    assert.isUrl(res.data[0].url)
+    assert.equal(res.response.headers['x-current-page'], 1)
+    assert.equal(res.response.headers['x-per-page'], 5)
   })
 })
 
 describe('"delete" method', function () {
   const gyazo = new Gyazo(process.env.GYAZO_TOKEN)
 
-  it('should delete uploaded image', function () {
+  it('should delete uploaded image', async function () {
     this.timeout(10000)
-    let resultId
-    return gyazo
-      .upload(imgPath)
-      .then((res) => {
-        resultId = res.data.image_id
-        return res.data.image_id
-      })
-      .then((id) => {
-        return gyazo.delete(id)
-      })
-      .then((res) => {
-        assert.equal(res.data.image_id, resultId)
-      })
+    const upRes = await gyazo.upload(imgPath)
+    const delRes = await gyazo.delete(upRes.data.image_id)
+    assert.equal(upRes.data.image_id, delRes.data.image_id)
   })
 })
