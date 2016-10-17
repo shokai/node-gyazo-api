5a288362c736212b3995404e4fc4d2610fc0f861
Sho Hashimoto <hashimoto@shokai.org>
2016-10-18 00:26:54 +0900

    follow standard

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,12 +1,9 @@
-"use strict";
-
-process.env.NODE_ENV = "test";
-if(!process.env.GYAZO_TOKEN){
-  console.error('ENV Var "GYAZO_TOKEN" required');
-  process.exit(1);
+if (!process.env.GYAZO_TOKEN) {
+  console.error('ENV Var "GYAZO_TOKEN" required')
+  process.exit(1)
 }
 
-import {assert} from "chai";
-assert.isUrl = function(str){
-  return assert.match(str, /^https?:\/\/.+/, "should be URL String");
+import {assert} from 'chai'
+assert.isUrl = function (str) {
+  return assert.match(str, /^https?:\/\/.+/, 'should be URL String')
 }
